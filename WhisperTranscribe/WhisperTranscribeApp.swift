2fcb8d11f1e3f4c5dd57d10821cfc08579099900
Abhishek Tyagi <abhishek4466@gmail.com>
2025-05-02 11:26:58 +0530

    Initial Commit

---
+++ WhisperTranscribe/WhisperTranscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperTranscribeApp.swift
+//  WhisperTranscribe
+//
+//  Created by Abhishek Tyagi on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisperTranscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
